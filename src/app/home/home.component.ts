0a557f9220398e813b1273a2bbb90b8b29b1a2f2
Saketh Sampath Vinay <sakethsampthvinay46@gmail.com>
2025-06-04 16:54:38 +0530

    Done Navbar and Hero Section

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NavbarComponent } from "../navbar/navbar.component";
+import { HeroComponent } from "./hero/hero.component";
+
+@Component({
+  selector: 'app-home',
+  imports: [NavbarComponent, HeroComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+}
