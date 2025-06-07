031b1ad9750f275be0f563dd1b7ad2d36e3c1fba
Saketh Sampath Vinay <sakethsampthvinay46@gmail.com>
2025-06-07 18:05:43 +0530

    done experience page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,15 @@ import { HotelsComponent } from './hotels/hotels.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { HotelBookingsComponent } from './hotel-bookings/hotel-bookings.component';
 import { HotelDetailsComponent } from './hotel-details/hotel-details.component';
+import { ExperiencesComponent } from './experiences/experiences.component';
+import { AboutComponent } from './about/about.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'rooms', component: HotelsComponent },
   { path: 'rooms/:id', component: HotelDetailsComponent },
   { path: 'bookings', component: HotelBookingsComponent },
+  { path: 'experience', component: ExperiencesComponent },
+  { path: 'about', component: AboutComponent },
   { path: '**', component: PageNotFoundComponent },
 ];
