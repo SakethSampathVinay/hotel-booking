72192ed58fcb5147798ba260c017a09537749fc2
Saketh Sampath Vinay <sakethsampthvinay46@gmail.com>
2025-06-07 16:50:33 +0530

    Done Hotel Bookings

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { HotelsComponent } from './hotels/hotels.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { HotelDetailsComponent } from './hotels/hotel-details/hotel-details.component';
+import { HotelBookingsComponent } from './hotel-bookings/hotel-bookings.component';
+import { HotelDetailsComponent } from './hotel-details/hotel-details.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'rooms', component: HotelsComponent },
   { path: 'rooms/:id', component: HotelDetailsComponent },
+  { path: 'bookings', component: HotelBookingsComponent },
   { path: '**', component: PageNotFoundComponent },
 ];
--- src/app/hotel-details/hotel-details.component.ts
+++ src/app/hotel-details/hotel-details.component.ts
@@ -1,13 +1,13 @@
 import { Component, OnInit, Output } from '@angular/core';
-import { roomsDummyData } from '../../../assets/assets';
-import { roomCommonData } from '../../../assets/assets';
-import { ActivatedRoute } from '@angular/router';
+import { roomsDummyData } from '../../assets/assets';
+import { roomCommonData } from '../../assets/assets';
+import { ActivatedRoute, RouterLink, RouterModule } from '@angular/router';
 import { CommonModule, NgIf } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-hotel-details',
-  imports: [NgIf, FormsModule, CommonModule],
+  imports: [NgIf, FormsModule, CommonModule, RouterLink, RouterModule],
   templateUrl: './hotel-details.component.html',
   styleUrl: './hotel-details.component.css',
 })
@@ -17,6 +17,10 @@ export class HotelDetailsComponent implements OnInit {
   hotel: any;
   selectedImage: string = '';
 
+  checkIn: string = '';
+  checkOut: string = '';
+  guest: number = 1;
+
   constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
@@ -24,5 +28,13 @@ export class HotelDetailsComponent implements OnInit {
     this.hotel = this.roomsDummyData.find((room) => room._id === id);
 
     this.selectedImage = this.hotel.images[0];
+  }
+
+  bookNow(): void {
+    if (this.checkIn !== '' && this.checkOut !== '' && this.guest >= 1) {
+      console.log('Booked Successfully');
+    } else {
+      console.log('Please fill all the details');
+    }
   }
 }
